0b75618cada686b9480055442ea0a1be574a21c4
Henrique Costa <henriquefcosta19@gmail.com>
2022-09-21 20:11:26 -0300

    update connect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://h-fcosta:rick1994@cluster0.sywfkwc.mongodb.net/alura-node?"
+  "mongodb+srv://h-fcosta:password@cluster0.sywfkwc.mongodb.net/alura-node?"
 );
 
 let db = mongoose.connection;
